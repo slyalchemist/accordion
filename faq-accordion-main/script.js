3a69d4e37000e3c69308fa8ee342e77ed9f547f8
slyalchemist <142651150+slyalchemist@users.noreply.github.com>
2025-02-03 17:32:23 -0800

    Finalize project
    Finalize project. Fix styles, add more symbols, add attributions.

--- faq-accordion-main/script.js
+++ faq-accordion-main/script.js
@@ -4,7 +4,6 @@ div.forEach((child) => {
     // and for each one we add a 'click' listener
     child.addEventListener("click", () => {
       console.log("clicked!");
-      child.classList.toggle("open");
 
       let description = child.querySelector(".description");
       if (description.style.display === "block") {
@@ -12,5 +11,9 @@ div.forEach((child) => {
       } else {
         description.style.display = "block";
       }
+
+      let sectionTitle = child.querySelector(".section-title");
+      let symbol = sectionTitle.querySelector(".symbol");
+      symbol.classList.toggle("minus");
     });
   });
